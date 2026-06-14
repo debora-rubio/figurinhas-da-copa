bed539a4152bbe6faa75b24c281741d770d12c91
debora-rubio <debora.rubio@fatec.sp.gov.br>
2026-06-14 13:55:13 -0300

    figurinha.py
    cria classe figurinha utilizando os atributos sugeridos pelo professor no pdf

---
+++ figurinha.py
@@ -0,0 +1,15 @@
+class Figurinha:
+# utilizando os atributos id, nome, país, posição e raridade conforme especificado.
+    def __init__(self, id, nome, pais, posicao, raridade):
+        self.id = id
+        self.nome = nome
+        self.pais = pais
+        self.posicao = posicao
+        self.raridade = raridade
+
+    def exibir(self):
+        print("ID:", self.id)
+        print("Nome:", self.nome)
+        print("País:", self.pais)
+        print("Posição:", self.posicao)
+        print("Raridade:", self.raridade)
