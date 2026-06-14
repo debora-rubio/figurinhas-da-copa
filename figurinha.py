1fc8e76035beddb79037bd12eb75706fa9213905
debora-rubio <debora.rubio@fatec.sp.gov.br>
2026-06-14 14:10:13 -0300

    figurinha.py
    adiciona o atributo quantidade

--- figurinha.py
+++ figurinha.py
@@ -1,5 +1,6 @@
 class Figurinha:
 # utilizando os atributos id, nome, país, posição e raridade conforme especificado.
+   
     def __init__(self, id, nome, pais, posicao, raridade):
         self.id = id
         self.nome = nome
@@ -7,9 +8,13 @@ class Figurinha:
         self.posicao = posicao
         self.raridade = raridade
 
+        # quantidade de cópias da figurinha
+        self.quantidade = 1
+
     def exibir(self):
         print("ID:", self.id)
         print("Nome:", self.nome)
         print("País:", self.pais)
         print("Posição:", self.posicao)
         print("Raridade:", self.raridade)
+        print("Quantidade:", self.quantidade)
